c5004295296d95e54685504d806011a44d4e5b27
EO <77565288+EOLSRepository@users.noreply.github.com>
2021-08-02 07:40:36 +0900

    Update bt3_ms1.py

--- ms3/bt3_ms1.py
+++ ms3/bt3_ms1.py
@@ -3,6 +3,7 @@
 # 조건 :  값 50 이하까지 출력
 
 def gugudan(num) :
+    print('-- [', num, '] 단 50이하값 홀수번째 출력 시작--')
     count = 1
     while count < 10 :
         if num * count  <= 50 :
@@ -11,5 +12,4 @@ def gugudan(num) :
     print ('---- 종료 ----')
 
 num = int(input('구구단 출력할 숫자는 무엇?: '))
-print('-- [', num, '] 단 50이하값 홀수번째 출력 시작--')
 gugudan(num)
